9f133364751075ffee55313d29d2092b79783c8e
Osama Madi <e2109762@cmaisonneuve.qc.ca>
2023-02-03 09:30:06 -0500

    Ajout de style pour le footer

--- footer.php
+++ footer.php
@@ -1,4 +1,7 @@
-<footer>
+<footer class="site__footer">
+    <section>YOOOO1</section>
+    <section>YOOOO2</section>
+    <section>YOOOO3</section>
 <h3> &copy; TIM 2023 Osama Madi</h3>
 </footer>
 <?php wp_footer(); ?>
