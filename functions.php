aacfb08d8c6e979dcd12833abc30c9816c40f470
Osama Madi <e2109762@cmaisonneuve.qc.ca>
2023-02-03 10:41:19 -0500

    fonction pour le style

--- functions.php
+++ functions.php
@@ -1,8 +1,24 @@
 <?php
+function ajouter_styles()
+{
     wp_enqueue_style(
-        'style-princ', 
-        get_template_directory_uri() . '/style.css', 
+        'style-princ',
+        get_template_directory_uri() . '/style.css',
         array(),
         filemtime(get_template_directory() . '/style.css'),
-        );   
-?>
+    );
+}
+add_action('wp_enqueue_scripts', 'ajouter_styles');
+
+
+if (!function_exists('enregistrement_nav_menu')) {
+
+    function enregistrement_nav_menu()
+    {
+        register_nav_menus(array(
+            'principal' => __('Menu Principal'),
+            'footer'  => __('Menu Pied De Page'),
+        ));
+    }
+    add_action('after_setup_theme', 'enregistrement_nav_menu', 0);
+}
