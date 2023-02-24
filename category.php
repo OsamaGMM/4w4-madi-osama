9ac1205e0b588d095d18c335c5d765af2d36d0e9
Osama Madi <e2109762@cmaisonneuve.qc.ca>
2023-02-23 21:05:23 -0500

    Ajout de logique qui gere les caracteres des titres des postes -> category.php

--- category.php
+++ category.php
@@ -6,10 +6,19 @@
     <?php if (have_posts()) :
         while (have_posts()) : the_post(); ?>
             <div class='article'>
-                <h1> <a href="<?= get_permalink(); ?>"> <?= get_the_title() ?> </a> </h1>
+                <h1> 
+                    <a href="<?= get_permalink(); ?>">
+                    <!-- Jai seulement deux cat pour l'instant
+                    donc je check si cest cours du tim ou note de cours
+                    et je gere les caractere a enlever dependament dans quelle page on est
+                    : TIM = 7 car; NDC = 3 car -->
+                     <?=(is_category('Cours du TIM')) ? substr(wp_trim_words(get_the_title(),10, ""), 7)
+                        : substr(wp_trim_words(get_the_title(),10, ""),3)?>
+                    </a> 
+                </h1>
                 <!-- the_content();?> -->
                 <?php //the_excerpt(); //affiche un resume de l'article?>
-                <p class="excerpt"><?= wp_trim_words(get_the_excerpt(), 7, "icon") ?>...</p>
+                <p class="excerpt"><?= wp_trim_words(get_the_excerpt(), 25, "icon") ?>...</p>
             </div>
     <?php endwhile;
     endif;
--- front-page.php
+++ front-page.php
@@ -6,13 +6,11 @@
     <?php if (have_posts()) :
         while (have_posts()) : the_post(); ?>
             <div class='article'>
-                <h1> <a href="<?= get_permalink(); ?>"> <?= get_the_title() ?> </a> </h1>
+                <h1> <a href="<?= get_permalink(); ?>"> <?= wp_trim_words( get_the_title(), 2 )?> </a> </h1>
                 <!-- the_content();?> -->
                 <?php //the_excerpt(); //affiche un resume de l'article?>
                 <p class="excerpt"><?= wp_trim_words(get_the_excerpt(), 7, "icon") ?>...</p>
-                
             </div>
-
     <?php endwhile;
     endif;
     ?>
