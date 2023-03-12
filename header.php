7d7c710af2bcac72a083b17a8d41119914fd24d1
OsamaGMM <osama_madi20@outlook.com>
2023-03-12 19:00:23 -0400

    Fin du menu burger

--- header.php
+++ header.php
@@ -13,26 +13,23 @@
     <header class="site__header">
         <section class="site__header__logo">
             <div class="logo"><?php the_custom_logo() ?></div>
+
             <div>
-            <input type="checkbox" id="checkbox-menu">
-
-            <?php wp_nav_menu(array(
-                'menu' => 'entete',
-                'container' => 'nav'
-            )) ?>
-
-
-            <?php get_search_form() ?>
-            <label class="burger" for="checkbox-menu"><img src="https://s2.svgbox.net/hero-outline.svg?ic=menu&color=000" width="20" height="20"></label>
-
+                <input type="checkbox" id="checkbox-menu">
+                <?php wp_nav_menu(array(
+                    'menu' => 'entete',
+                    'container' => 'nav'
+                )) ?>
+                <?php get_search_form() ?>
+                <label class="burger" for="checkbox-menu"><img src="https://s2.svgbox.net/hero-outline.svg?ic=menu&color=000" width="20" height="20"></label>
             </div>
 
         </section>
 
     </header>
-    
-    <?php 
-        if ( ! is_front_page()){
-        get_template_part("template-parts/aside"); 
-        }
+
+    <?php
+    if (!is_front_page()) {
+        get_template_part("template-parts/aside");
+    }
     ?>
