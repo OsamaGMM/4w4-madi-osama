faae9d1d89da0ff8e9280c1d8178a7d2d217d523
OsamaGMM <osama_madi20@outlook.com>
2023-04-24 13:27:06 -0400

    Ajout de la page evenement

--- front-page.php
+++ front-page.php
@@ -1,16 +1,25 @@
 <?php get_header(); ?>
 
+<section >
+        <?php wp_nav_menu(array(
+            "menu" => "evenement",
+            "container" => "nav"
+        )); ?>
+    </section>
+
 <section class="liste-articles">
-<?php
-        if (have_posts()): 
-            while (have_posts()) : the_post(); 
-                    $ma_categorie = "4w4";
-                    if (in_category('galerie')){
-                        $ma_categorie = "galerie";
-                    }
-                 get_template_part("template-parts/categorie",$ma_categorie);
-             endwhile;
-        endif;
+
+
+    <?php
+    if (have_posts()) :
+        while (have_posts()) : the_post();
+            $ma_categorie = "4w4";
+            if (in_category('galerie')) {
+                $ma_categorie = "galerie";
+            }
+            get_template_part("template-parts/categorie", $ma_categorie);
+        endwhile;
+    endif;
     ?>
 </section>
 
